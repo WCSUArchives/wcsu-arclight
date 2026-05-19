d299370d31ad02c5f34529c4f706e0e5cf7464b5
archivalGrysbok <archivalGrysbok@github.com>
2026-05-19 15:54:53 -0400

    Add missing Rails core files

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require_relative "application"
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ENV['REPOSITORY_FILE'] = "/home/arclight/wcsu-arclight/config/repositories.yml"
